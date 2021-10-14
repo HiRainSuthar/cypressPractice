fbc0c7c993e4a47e4e0012efa37961774a1bed76
Hiren <hsuthar@mobiquityinc.com>
2021-10-14 12:40:02 +0530

    development for teste cases of inbox

--- cypress/integration/tests/inbox.spec.js
+++ cypress/integration/tests/inbox.spec.js
@@ -1,21 +1,38 @@
+import registerPage from '../pageObjects/registerpageObjects'
+import inboxPage from '../pageObjects/inboxObjects'
 
 describe('Tests for inbox tab',function(){
     before(function(){
-        cy.visit('https://nowvue.live/vue-by-nowevents-demo-A000180/register')
-        cy.wait(7000)
-        cy.xpath("(//input[@placeholder='Enter your email'])[1]").type("test@test.com")
-        cy.wait(500)
-        cy.xpath("//span[contains(text(),'Access now')]").click()
-        cy.wait(5000)
-        cy.xpath("//div[@id='navbarCollapse']/links[1]//a[contains(text(),'Inbox')]").click()
-        cy.wait(20000)
+        registerPage.goToDashboard()
+        cy.wait(1000)
     })
     after(function(){
-        cy.xpath("//div[@id='navbarCollapse']/links[2]/ul/li[1]/a/img").click()
-        cy.xpath("//div[@id='navbarCollapse']//div//a[contains(text(),'Logout')]").click()
+        registerPage.logout()
     })
-    it('Edit personal Profile (Via Network)', function(){
-        cy.log("on network tab")
-        //cy.get("#createNewLoungeRoom").click()
+    it('Enter inbox', function(){
+        cy.xpath(inboxPage.inboxLinkOnNavBar).click()
+        cy.get(inboxPage.inboxBarInRightSide).should('be.visible')
+        cy.xpath(inboxPage.inboxBarCloseBtn).click()
+        cy.wait(1000)
+    })
+    it('Send message for Conf chat', function(){
+        cy.xpath(inboxPage.inboxLinkOnNavBar).click()
+        cy.get(inboxPage.inboxBarInRightSide).should('be.visible')
+        cy.xpath(inboxPage.firstChatRoomOnBar).click();
+        cy.wait(1000)
+        cy.get(inboxPage.chatBox).should('be.visible')
+        cy.get(inboxPage.typeMessageInputBox).type('automated hello')
+        cy.wait(1000)
+        cy.get(inboxPage.chatMessage).last().should('have.text', 'automated hello')
+        cy.xpath(inboxPage.closeChatBox).click()
+    })
+    it('Delete Chat', function(){
+        cy.xpath(inboxPage.inboxLinkOnNavBar).click()
+        cy.get(inboxPage.inboxBarInRightSide).should('be.visible')
+        cy.xpath(inboxPage.firstChatRoomOnBar).click();
+        cy.wait(1000)
+        cy.get(inboxPage.chatBox).should('be.visible')
+        cy.xpath(inboxPage.closeChatBox).click();
+        cy.get(inboxPage.chatBox).should('not.be.visible')
     })
 })
